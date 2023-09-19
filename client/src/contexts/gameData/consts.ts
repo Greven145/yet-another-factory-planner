de7bed3f2595c9827ee0c2910dcd9a8ea28d1f86
Mark Gottselig <mark.gottselig@gmail.com>
2023-09-19 00:35:35 -0600

    Incorporate changes for more indepth output from @ nikolatomasevicpro in PR #13

--- client/src/contexts/gameData/consts.ts
+++ client/src/contexts/gameData/consts.ts
@@ -2,7 +2,7 @@ export const GV_U5 = 'U5';
 export const GV_U6 = 'U6';
 export const GV_U7 = 'U7';
 export const GV_U8 = 'U8';
-export const DEFAULT_GAME_VERSION = GV_U8;
+export const DEFAULT_GAME_VERSION = GV_U7;
 export const LEGACY_GAME_VERSION = GV_U5;
 
 export const SHARE_QUERY_PARAM = 'factory';
--- client/src/contexts/production/types.ts
+++ client/src/contexts/production/types.ts
@@ -42,3 +42,13 @@ export type FactoryOptions = {
   allowedRecipes: RecipeSelectionMap,
   nodesPositions: NodeInfo[],
 };
+
+export type NodeInformation = {
+  x: number,
+  y: number,
+  key: string,
+}
+
+export type FactoryGraphChanges = {
+  nodes: NodeInformation[],
+}
